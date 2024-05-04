565cbaa73eb98c58213aac9a65a27e75e5b859f8
Manojkumar0721 <manu07y@gmail.com>
2024-05-04 20:48:10 +0530

    adding variable file

---
+++ Variable.java
@@ -0,0 +1,136 @@
+public class Variable{
+    public static void main(String[] args){
+
+    // byte data type examples 
+
+        byte wheelsInBike = 2;
+        byte wheelsInCar = 4;
+        byte fingersInHand = 10;
+        byte keysInKeyboard = 108;
+        byte camerasInPhone = 4;
+        byte englishAlphabets = 26;
+        byte phoneBatteryPersent = 100;
+        byte legsInChair = 4;
+        byte daysInWeek = 7;
+        byte daysInMonth = 30;
+
+        System.out.println(wheelsInBike);
+        System.out.println(wheelsInCar);
+        System.out.println(fingersInHand);
+        System.out.println(keysInKeyboard);
+        System.out.println(camerasInPhone);
+        System.out.println(englishAlphabets);
+        System.out.println(phoneBatteryPersent);
+        System.out.println(legsInChair);
+        System.out.println(daysInWeek);
+        System.out.println(daysInMonth);
+
+    // short data type examples 
+
+        short peoplesInVillage = 2000;
+        short daysInYear = 365; 
+        short angleOfCircle = 360;
+        short studentsInClass = 150;
+        short peoplesInTeater = 500;
+        short carInParkingSlot = 200;
+        short rpmRedLineInBike = 12000;
+        short hpOfGtr = 1500;
+        short salaryPerDay = 10000;
+        short distanceFromJammuToKarnataka = 2424;
+
+
+        System.out.println(peoplesInVillage);
+        System.out.println(daysInYear);    
+        System.out.println(angleOfCircle);
+        System.out.println(studentsInClass);
+        System.out.println(peoplesInTeater);
+        System.out.println(carInParkingSlot);
+        System.out.println(rpmRedLineInBike);
+        System.out.println(hpOfGtr);
+        System.out.println(salaryPerDay);
+        System.out.println(distanceFromJammuToKarnataka);
+
+    
+    // int data type examples 
+       
+
+       int rangeRoverPrice = 20000000;
+       int distanceFromJammuToKanyakumari = 3405;
+       int nmStadiumSeatingCapacity = 132000;
+       int populationOfKarnataka = 67833000;
+       int mustangePrice = 15000000;
+
+
+       System.out.println(rangeRoverPrice);
+       System.out.println(distanceFromJammuToKanyakumari);
+       System.out.println(nmStadiumSeatingCapacity);
+       System.out.println(populationOfKarnataka);
+       System.out.println(mustangePrice);
+
+    // long data type examples
+
+
+    long distance = 1000000000L;
+    long balance = 1000000000L;
+    long number = 1876545678L;
+    long aadharNumber = 1977880004L;
+    long num = 1648562354L;
+
+
+    System.out.println(distance);
+    System.out.println(balance);
+    System.out.println(number);
+    System.out.println(aadharNumber);
+    System.out.println(num);
+
+// float data type examples
+
+
+float amount = 9.3567f;
+float length = 2.2786f;
+float angle = 3.16788f;
+
+
+System.out.println(amount);
+System.out.println(length);
+System.out.println(angle);
+
+
+
+//double data type examples
+
+double areaInAcres = 2.23444d;
+
+System.out.println(areaInAcres);
+
+
+
+//char data type examples 
+
+char grade = 'A';
+char room = 'A';
+char section = 'B';
+
+
+System.out.println(grade);
+System.out.println(room);
+System.out.println(section);
+
+
+//boolean data type examples
+
+boolean isJavaFun = true;
+boolean isFishTasty = true;
+boolean youHaveCar = false;
+boolean youHaveBike = false;
+
+System.out.println(isJavaFun);
+System.out.println(isFishTasty);
+System.out.println(youHaveCar);
+System.out.println(youHaveBike);
+
+
+
+    }
+
+}
