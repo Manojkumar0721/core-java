b0405858c75059b2992c477e24ccb4016e0ef9e9
Manojkumar0721 <manu07y@gmail.com>
2024-05-04 20:42:12 +0530

    adding program file

---
+++ Program.java
@@ -0,0 +1,8 @@
+public class Program{
+    public static void main(String[] args){
+        String name = "Manoj";
+        System.out.println(name);
+        name = "Sarvam";
+        System.out.println(name);
+    }
+}
