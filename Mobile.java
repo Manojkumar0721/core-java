e4914a66ca707fa5a1023a24dab33b6b3d9f4ac7
Manojkumar0721 <manu07y@gmail.com>
2024-05-27 19:27:35 +0530

    adding mobile file

---
+++ Mobile.java
@@ -0,0 +1,38 @@
+public class Mobile{
+
+	public static void main(String[] args) {
+
+		String mobile1 = "Redmi Note 9";
+		String processor1 = "Octa-core Helio G85";
+		String color1 = "Aqua Green";
+		String operatingSystem1 = "MIUI 10";
+		String display1 = "DotDisplay";
+		String simCard1 = "Dual SIM Card";
+		
+        String mobile2 = new String("Samsung S23 FE 5G");
+        String processor2 = new String("Qualcomm");
+        String color2 = new String("Purpul");
+        String operatingSystem2 = new String("Android 10.0");
+        String display2 = new String("Passionate Galaxy");
+        String simCard2 = new String("Dual SIM Card");
+
+
+		System.out.println("-----------------");
+		System.out.println(mobile1);
+        System.out.println(processor1);
+        System.out.println(color1);
+        System.out.println(operatingSystem1);
+        System.out.println(display1);
+        System.out.println(simCard1);
+
+
+		System.out.println("------------------");
+        System.out.println(mobile2);
+        System.out.println(processor2);
+        System.out.println(color2);
+        System.out.println(operatingSystem2);
+        System.out.println(display2);
+        System.out.println(simCard2);
+
+	}
+}
