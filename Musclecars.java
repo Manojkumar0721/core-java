092198ad02b6f312f6fe6ad9cf981da4981ce43c
Manojkumar0721 <manu07y@gmail.com>
2024-07-06 19:06:18 +0530

    Declaration and Invoking The Mthods

---
+++ Musclecars.java
@@ -0,0 +1,41 @@
+public class Musclecars{
+    public static void main(String[] args){
+        System.out.println("--------------------**--------------------");
+        System.out.println("            AMERICAN MUSCLE CARS          ");
+        System.out.println("--------------------**--------------------");
+
+        ford();
+        dodge();
+        Chevrolet();
+
+
+    }
+
+    public static void ford(){
+        System.out.println("             * Ford Mustang *              ");
+        System.out.println("Production : 1965-2024");
+        System.out.println("Engine     : 4951cc");
+        System.out.println("Maximum Power  : 396 HP @ 6500 rpm");
+        System.out.println("Maximum Torque : 515Nm  @ 4250 rpm");
+        System.out.println("____________________________________________");
+    }
+
+    public static void dodge(){
+        System.out.println("             * Dodge Challenger *              ");
+        System.out.println("Production : 1969-2023");
+        System.out.println("Engine     : 6162cc");
+        System.out.println("Maximum Power  : 707 HP @ 6000 rpm");
+        System.out.println("Maximum Torque : 880Nm @ 4000 rpm");
+        System.out.println("____________________________________________");
+    }
+    public static void Chevrolet(){
+        System.out.println("             * Chevrolet Corvette *              ");
+        System.out.println("Production : 1969-2023");
+        System.out.println("Engine     : 6161cc");
+        System.out.println("Maximum Power  : 455 HP @ 6500 rpm");
+        System.out.println("Maximum Torque : 624Nm  @ 3800 rpm");
+        System.out.println("____________________________________________");
+    }
+
+    
+}
