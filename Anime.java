8ea5cd4c6cebb7c58eba8948bc79f0ac2b3abc98
Manojkumar0721 <manu07y@gmail.com>
2024-07-06 18:56:56 +0530

    Declaration and Invokin The Mthods

---
+++ Anime.java
@@ -0,0 +1,47 @@
+public class Anime{
+    
+    
+    public static void writer(){
+        System.out.println("Manga Artist : Masashi Kishimoto");
+    }
+
+    public static void hokage(){
+        System.out.println("----------**----------");
+        System.out.println("First Hokage : Hashirama Senju");
+        System.out.println("Second Hokage : Tobirama Senju");
+        System.out.println("Third Hokage :  Hiruzen Sarutobi");
+        System.out.println("Fourth Hokage : Minato Namikaze");
+        System.out.println("Fifth Hokage : Tsunade");
+        System.out.println("Sixth Hokage : Kakashi Hatake");
+        System.out.println("Seventh Hokage : Naruto Uzumaki");
+        System.out.println("Eighth Hokage : Shikamaru Nara");
+        System.out.println("----------**----------");
+    }
+
+    public static void villages(){
+        System.out.println("----------**----------");
+        System.out.println("Hidden Leaf Village");
+        System.out.println("Hidden Sand Village");
+        System.out.println("Hidden Rock Village");
+        System.out.println("Hidden Mist Village");
+        System.out.println("Hidden Cloud Village");
+        System.out.println("Hidden Rain Village");
+        System.out.println("Hidden Watterfall Village");
+        System.out.println("Hidden Sound Village");
+        System.out.println("----------**----------");
+    }
+    public static void main(String[] args){
+        writer();
+        System.out.println("Anime : Naruto");
+
+        writer();
+        hokage();
+        
+    
+
+    }
+    
+
+
+
+}
