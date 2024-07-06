2855b33127104f3805615ca47b50184cd084f77c
Manojkumar0721 <manu07y@gmail.com>
2024-07-06 19:01:57 +0530

    Declaration and Invoking The Mthods

---
+++ JdmCar.java
@@ -0,0 +1,59 @@
+public class JdmCar{
+    public static void main(String[] args){
+        System.out.println("--------------------**--------------------");
+        System.out.println("                 JDM Cars         ");
+        System.out.println("--------------------**--------------------");
+        
+        carBrands();
+        carNames();
+        luxuriousCar();
+
+    }
+    public static void carBrands(){
+        System.out.println("              JDM CAR BRANDS           ");
+        System.out.println("             ~~~~~~~~~~~~~~~~          ");
+        System.out.println("--> NISSAN");
+        System.out.println("--> TOYOTA");
+        System.out.println("--> MITSUBISHI");
+        System.out.println("--> MAZDA");
+        System.out.println("--> SUBARU");
+        System.out.println("--> LEXUS");
+        System.out.println("--> HONDA");
+        System.out.println("--> DATSUN");
+        System.out.println("____________________________________________");
+
+    }
+
+    public static void carNames(){
+        System.out.println("                JDM CARS               ");
+        System.out.println("               ~~~~~~~~~~              ");
+        System.out.println("--> NISSAN GTR R32,R33,R34,R35");
+        System.out.println("--> TOYOTA SUPRA MK4");
+        System.out.println("--> MITSUBISHI EVO");
+        System.out.println("--> MAZDA RX7");
+        System.out.println("--> SUBARU BRZ");
+        System.out.println("--> LEXUS LFA");
+        System.out.println("--> HONDA CIVIC");
+        System.out.println("--> DATSUN 240Z");
+        System.out.println("____________________________________________");
+
+    }
+
+    public static void luxuriousCar(){
+        System.out.println("           LEXURIOUS JDM CAR            ");
+        System.out.println("          ~~~~~~~~~~~~~~~~~~~~          ");
+        System.out.println("--> Toyota Century");
+        System.out.println("--> Lexus LX 600");
+        System.out.println("--> Infiniti QX 600");
+        System.out.println("--> Acura RLX Sports");
+        System.out.println("--> Mazda6 Signature");
+        System.out.println("--> Acura TLX Type-S");
+        System.out.println("--> Infiniti Q70L");
+        System.out.println("--> Nissan President");
+        System.out.println("--> Lexus LC 500");
+        System.out.println("--> Nissan Cedric");
+        System.out.println("____________________________________________");
+
+    }
+
+}
