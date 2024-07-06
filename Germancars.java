b5c51ffb259adb8c372c477a6ec21d9d082eafbe
Manojkumar0721 <manu07y@gmail.com>
2024-07-06 19:04:14 +0530

    Declaration and Invoking The Mthods

---
+++ Germancars.java
@@ -0,0 +1,51 @@
+public class Germancars{
+    public static void main(String[] args){
+        System.out.println("--------------------**--------------------");
+        System.out.println("               GERMAN CARS                ");
+        System.out.println("--------------------**--------------------");
+
+        carbrands();
+        carTuning();
+        carNames();
+
+
+
+    }
+
+    public static void carbrands(){
+        System.out.println("             German Car Brands             ");
+        System.out.println("             ~~~~~~~~~~~~~~~~~~            ");
+        System.out.println("--> PORSCHE");
+        System.out.println("--> BMW");
+        System.out.println("--> MERCEDES-BENZ");
+        System.out.println("--> AUDI");
+        System.out.println("____________________________________________");
+
+    }
+
+    public static void carTuning(){
+        System.out.println("            German Car Tuning             ");
+        System.out.println("           ~~~~~~~~~~~~~~~~~~~            ");
+        System.out.println("--> Brabus");
+        System.out.println("--> Carlsson");
+        System.out.println("--> Alpina");
+        System.out.println("--> Mansory");
+        System.out.println("--> 9ff");
+        System.out.println("--> RUF");
+        System.out.println("--> TechArt");
+        System.out.println("--> ABT");
+        System.out.println("____________________________________________");
+
+    }
+
+    public static void carNames(){
+        System.out.println("                Cars                      ");
+        System.out.println("               ~~~~~~                     ");
+        System.out.println("BMW M5 Compition");
+        System.out.println("Porsche 911 GT3");
+        System.out.println("Mercedes-Benz G63 ");
+        System.out.println("Mercedes-Benz SLR McLaren");
+
+
+    }
+}
