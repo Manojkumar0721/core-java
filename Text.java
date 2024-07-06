fe99a49022b9e3d266824bc6471f40582bc589db
Manojkumar0721 <manu07y@gmail.com>
2024-07-06 18:45:24 +0530

    Variabels and Reassigning

---
+++ Text.java
@@ -0,0 +1,56 @@
+public class Text{
+    public static void main(String[] args){
+        String firstName = "Manoj";
+        System.out.println(firstName);
+        firstName = "SARVAM";                //reassigning
+        System.out.println(firstName);
+        String secondName = "Kumar";
+        System.out.println(secondName);
+        String fullName = "Manoj Kumar";
+        System.out.println(fullName);
+        String car = "GTR";
+        System.out.println(car);
+        car = "NISSAN";                      //reassigning
+        System.out.println(car);
+        String bike = "Kavasaki";
+        System.out.println(bike);
+        bike = "DUKATI";                     //reassigning
+        System.out.println(bike);
+        String place ="India,Japan,German,America,Itali";
+        System.out.println(place);
+        String message = "Determination naver betrays you";
+        System.out.println(message);
+        String song = "Happy nation";
+        System.out.println(song);
+        String team = "RCB";
+        System.out.println(team);
+        int number = 07;
+        System.out.println(number);
+        String firstAnime = "Naruto";
+        System.out.println(firstAnime);
+        firstAnime = "DEATH NOTE";             //reassigning
+        System.out.println(firstAnime);
+        String friendName = "Teja";
+        System.out.println(friendName);
+        String sister = "Dhanya";
+        System.out.println(sister);
+        String brother = "Harsha";
+        System.out.println(brother);
+        String father = "Prasanna";
+        System.out.println(father);
+        String mother = "Lakshmi";
+        System.out.println(mother);
+        String jdmCar = "Toyota,Nissan,Honda,Mitsubishi,Mazda,Lexus";
+        System.out.println(jdmCar);
+        String germanCar = "BMW,Porsche,Mercedes-Benz";
+        System.out.println(germanCar);
+        String americanCar = "Ford,Chevrolet,Dodge";
+        System.out.println(americanCar);
+        String indianCar = "Tata,Mahendra";
+        System.out.println(indianCar);
+
+  
+
+
+    }
+}
